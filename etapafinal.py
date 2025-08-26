3ab7088b75cb8e7d4a3f3a0f8c960d1f6419e4bb
amanda lacerda <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-08-26 12:17:57 +0000

    etapafinal.py

---
+++ etapafinal.py
@@ -0,0 +1,17 @@
+def cadastrar_produto():
+  print("============================================")
+  print(" Bem-vindo(a) à Loja de vestido da Maria 🛍️ ")
+  print("============================================\n")
+    
+nome = input("Digite o nome do vestido: ")
+preco = float(input("Digite o preço do vestido  (em R$): "))
+categoria = input("Digite a categoria do vestido : ")
+
+if  ("formatar_nome"(nome), preco, categoria):
+  
+  def salvar_produto(produto):
+      with open("vestido.txt", "a", encoding="utf-8") as arquivo:linha = f"{vestido[0]};{vestido[1]};{vestido[2]}\n" 
+      arquivo.write(linha)
+      def listar_produtos():  
+         def listar_produtos():
+    
